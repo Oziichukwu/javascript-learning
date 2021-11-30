990214768ac125fdbda06ccc1ad7298b13005be2
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-30 13:43:39 +0100

    first commit

---
+++ dom/app.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById('task-title'));
+
+// change stying
+
+document.getElementById('task-title').style.color = '#333';
+
+const items = document.getElementsByClassName('')
