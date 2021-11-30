84b665bbc14c6385cf0e67a27bcd58d0b7c2c6e0
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-30 19:51:07 +0100

    more work

--- dom/app.js
+++ dom/app.js
@@ -1,7 +1,57 @@
-console.log(document.getElementById('task-title'));
+const form = document.querySelector('#task-form');
 
-// change stying
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
 
-document.getElementById('task-title').style.color = '#333';
+// load all event listeners
+loadEventListerners();
 
-const items = document.getElementsByClassName('')
+// load all event listeners
+function loadEventListerners() {
+
+    // add task event
+    form.addEventListener('submit', addTask);
+}
+
+// add task
+function addTask(e) {
+
+    if(taskInput.value === ''){
+        alert('Add a task');
+    }
+
+    // create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item'
+    //create text node and append to li
+
+    li.appendChild(document.createTextNode(taskInput.value));
+    // create new link element
+    const link = document.createElement('a');
+    // add class
+    link.className = 'delete-item secondary-content';
+    // add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // append the link to li
+    li.appendChild(link);
+
+    // append li to ul
+    taskList.appendChild(li);
+
+    //clear input
+    taskInput.value = '';
+
+
+    e.preventDefault();
+}
+
+
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+
+        console.log(e.target)
+    }
+}
